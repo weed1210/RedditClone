5d167845fd6491d7b14d4c9e7ebb8bca1fae1c9a
weed1210 <vendetta9z147@gmail.com>
2024-06-04 01:28:42 +0700

    Update Program.cs

--- Reddit.API/Program.cs
+++ Reddit.API/Program.cs
@@ -30,11 +30,11 @@ builder.Services.AddSignalR();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
